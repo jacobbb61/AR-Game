f746822afbda39eabdc55deb97e66182d44913fc
jacobbb61 <mcconnelljacob61@gmail.com>
2022-12-09 23:49:51 +0000

    weewoooooo

--- AR-UnityFolder/Assets/Scripts/Faces/Face3.cs
+++ AR-UnityFolder/Assets/Scripts/Faces/Face3.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class Face3 : MonoBehaviour
 {
     GameObject Ball;
-    Animator Anim;
+    public Animator AnimDoor2;
+    public Animator AnimDoor1;
     public Animator WireAnim;
 
-    public GameObject Plate1, Wire1;
+    public GameObject Plate1, Wire1, Plate2;
 
     public bool open = false;
 
     void Start()
     {
         Ball = GameObject.FindGameObjectWithTag("Ball");
-        Anim = GetComponentInChildren<Animator>();
+
     }
 
     void Update()
     {
-        if (Plate1.GetComponent<PressurePlate>().On >= 1 && Wire1.GetComponent<WireDetect>().on == true) { open = true; } else { open = false; }
-        if (open) { Anim.SetBool("Open", true); } else { Anim.SetBool("Open", false); }
+        if (Plate2.GetComponent<PressurePlate>().On >= 1) { open = true; } else { open = false; }
+        if (Plate1.GetComponent<PressurePlate>().On >= 1 && Wire1.GetComponent<WireDetect>().on) { AnimDoor2.SetBool("Open", true); } else { AnimDoor2.SetBool("Open", false); }
+        if (open) { AnimDoor1.SetBool("Open", true); } else { AnimDoor1.SetBool("Open", false); }
+
     }
 }
