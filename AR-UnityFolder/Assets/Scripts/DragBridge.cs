51bde6ec5004f2e8d098030309ceed7e4b8d14bd
jacobbb61 <mcconnelljacob61@gmail.com>
2023-01-05 21:18:52 +0000

    ARGA

--- AR-UnityFolder/Assets/Scripts/DragBridge.cs
+++ AR-UnityFolder/Assets/Scripts/DragBridge.cs
@@ -7,14 +7,22 @@ public class DragBridge : MonoBehaviour
 
     GameObject Player;
     GameObject Level;
-    public Transform Pos1, Pos2;
+    public Transform Pos1, Pos2, goingPos, fromPos;
     public bool AtPos1 = false;
+    public bool move = false;
 
+
+    
 
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Level = GameObject.FindGameObjectWithTag("Level");
+    }
+
+    private void Update()
+    {
+       // if (move) { Vector3.Lerp(fromPos.position, goingPos.position, Time.deltaTime); }
     }
 
 
@@ -23,7 +31,18 @@ public class DragBridge : MonoBehaviour
         Player.GetComponent<NavMeshAgent>().enabled = false;
 
         if (AtPos1) { transform.position = Pos2.position; AtPos1 = false; }
-        else { transform.position = Pos1.position; AtPos1 = true; }
+        else { transform.position = Pos1.position; AtPos1 = true; }   
+        
+        if (AtPos1)
+        {
+            fromPos = transform;
+            goingPos = Pos2;
+        }
+        else
+        {
+            fromPos = transform;
+            goingPos = Pos1;
+        }
 
 
         GameObject.FindGameObjectWithTag("MainFloor").GetComponent<NavigationBaker>().ReBuildMesh();
